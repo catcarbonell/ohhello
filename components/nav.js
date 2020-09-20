f54377a563256d82b33b05e27828b802a6d06afa
catcarbonell <hello@catcarbn.com>
2020-09-20 00:24:31 -0700

    Able to hide/show nav menu. Need to animate .

--- components/nav.js
+++ components/nav.js
@@ -1,16 +1,29 @@
-// import {useState, useEffect, useRef} from 'react'
+import {useState} from 'react'
 import Link from 'next/link'
 import NavItem from './layout/NavItem'
 
 const Nav = () => {
+    const [state, setState] = useState(true);
 
+    const toggleMenu = () => {
+        let menu = document.getElementById('menu')
+        setState(!state)
+        if(menu.classList.contains('hidden')){
+
+            menu.classList.remove('hidden');
+        } else {
+            menu.classList.add('hidden')
+        }
+    };
+     
 
     return(
         <>
-        <div id="menu-btn" className="flex self-start m-10 fixed z-20">
-            <h3 className="cursor-pointer hover:bg-custom-blk">Menu</h3>
+        <div id="menu-btn" className="flex self-end m-10 fixed z-20">
+            <h3 onClick={toggleMenu} className="cursor-pointer hover:bg-custom-shadow p-2">Menu</h3>
         </div>
-        <nav id="main-nav" className="fixed h-screen bg-custom-blk bg-opacity-75 shadow-md md:text-right text-center md:w-1/6 w-full z-30 absolute flex flex-col place-self-start font-bold justify-center">         
+
+        <nav id="menu" className="hidden fixed bg-custom-blk bg-opacity-75 shadow-md md:text-left text-center md:w-1/6 w-full z-30 absolute flex flex-col place-self-end font-bold justify-center">         
             <div className="mr-2">
                 <Link href="/"><a><NavItem text="Home" /></a></Link>
                 <Link href="/blog"><a><NavItem text="Blog" /></a></Link>
@@ -18,7 +31,7 @@ const Nav = () => {
                 <Link href="/store"><a><NavItem text="Store" /></a></Link>
                 <NavItem text="About" />
                 <NavItem text="Contact" />
-                <NavItem text="&larr; Close Menu" />
+                <div onClick={toggleMenu}><NavItem text="Close Menu &rarr;" /></div>
             </div>
         </nav>
         </>
--- utils/animate.js
+++
@@ -1,6 +0,0 @@
-
-import { gsap } from "gsap";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
