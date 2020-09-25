50f5a42a3ed24d0d7a589564bf39396c3bb2226d
catcarbonell <hello@catcarbn.com>
2020-09-25 09:26:16 -0700

    Fixed stack section

--- components/layout/stackItem.js
+++ components/layout/stackItem.js
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function StackItem(props){
     return(
-        <div className="border-2 border-custom-flamingo mb-2 flex flex-row flex-no-wrap">
-            <div className="border-2 h-8 w-8">
+        <div className="mb-2 flex flex-row flex-no-wrap">
+            <div className="h-8 w-8">
               <FontAwesomeIcon icon={props.icon} />
             </div>
-            <h3 className="ml-2 border-2">{props.text}</h3>
+            <h3 className="ml-2">{props.text}</h3>
         </div>
     )
 }
