abeb594b71e330088d17095365e0c1cab663c476
catcarbonell <hello@catcarbn.com>
2020-09-25 09:23:33 -0700

    Still fixing about page

--- components/layout/stackItem.js
+++ components/layout/stackItem.js
@@ -4,7 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function StackItem(props){
     return(
         <div className="border-2 border-custom-flamingo mb-2 flex flex-row flex-no-wrap">
-            <FontAwesomeIcon className="md:w-6" icon={props.icon} />
+            <div className="border-2 h-8 w-8">
+              <FontAwesomeIcon icon={props.icon} />
+            </div>
             <h3 className="ml-2 border-2">{props.text}</h3>
         </div>
     )
