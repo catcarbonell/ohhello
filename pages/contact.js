35ae737a28693f0bde79de97fe8cafc077721814
catcarbonell <hello@catcarbn.com>
2020-09-27 18:07:27 -0700

    added load animations to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,13 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
+import { gsap } from 'gsap/dist/gsap';
 import {InlineWidget} from 'react-calendly'
 import Socials from '../components/layout/Socials'
 
 export default function Contact(){
+    useEffect(()=>{
+        gsap.fromTo('.contact-section',{x:1000, autoAlpha:0}, {x:0, autoAlpha:1, duration: 1, stagger:0.5})
+    }, [])
     return(
         <>
             <Head>
@@ -12,17 +17,17 @@ export default function Contact(){
 
             <div className="mx-auto w-5/6 md:w-full">
                 
-                <div className="mb-24">
+                <div className="contact-section mb-24">
                     <h2 className="mb-4 text-custom-lavender">Add/Follow me!</h2>
                     <Socials/>
                 </div>
 
-                <div className="text-xl mb-24">
+                <div className="contact-section text-xl mb-24">
                     <h2 className="mb-4 text-custom-lavender">Email me!</h2>
                     <p>&rarr; hello (at) catcarbn.com</p>
                 </div>
 
-                <div id="calendly" className="text-lg mb-24">
+                <div id="calendly" className="contact-section text-lg mb-24">
 
                     <h2 className="text-custom-lavender">Book a <span className="uppercase">free</span> 30 min. intro call!</h2>
                     <p className="mb-10">...or book a 1 or 2 hour consultation <span className="font-bold">right now! &darr;</span></p>
