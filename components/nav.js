7bac05c4fec5a1bbad1e371dd77bdd69ae9fbf49
catcarbonell <hello@catcarbn.com>
2020-09-28 20:45:29 -0700

    fixing nav menu

--- components/nav.js
+++ components/nav.js
@@ -27,7 +27,7 @@ const Nav = () => {
      
 
     return(
-        <div className="overflow-hidden fixed z-30 w-screen flex flex-row py-2 md:pt-2 justify-center items-center bg-custom-blk bg-opacity-50">
+        <div className="fixed z-30 w-screen flex flex-row py-2 md:pt-2 justify-center items-center bg-custom-blk bg-opacity-50">
             
             <div className="flex items-center justify-start ml-4 w-1/2">
                 <Link href="/"><a>
