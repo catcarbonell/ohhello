538f1cf0c24802acd2ed790bf2f76f6e24d00905
catcarbonell <hello@catcarbn.com>
2020-09-29 17:04:55 -0700

    Fixed twitter meta tags

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -24,6 +24,10 @@ const PostPage = (props) => {
 
                 <meta name="twitter:card" content="summary" key="twcard" />
                 <meta name="twitter:creator" content="@catvscode" key="twhandle" />
+                <meta property='twitter:title' content={props.post.title} />
+                <meta property='twitter:image' content={myUrl} />
+                <meta name="twitter:card" content={props.post.feature_image} />
+
                 
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta charSet="utf-8" />
