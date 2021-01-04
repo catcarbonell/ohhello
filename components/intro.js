ec051948f1874f55f2bd9110ed25b616ac87dd26
catcarbonell <hello@catcarbn.com>
2021-01-03 17:49:15 -0800

    completely stripped my portfolio

--- components/intro.js
+++ components/intro.js
@@ -6,12 +6,10 @@ const Intro = () =>{
       <div className="intro w-full lg:w-1/2 md:max-w-xl h-xxs md:mx-auto mt-20 mb-20 flex flex-row bg-custom-blk bg-opacity-75 rounded-lg">
       
         <div className="md:w-1/2 p-6 flex flex-col justify-center align-center">
-        <a className="hover:bg-transparent"> 
             <h2 className="text-custom-sakura hover:text-custom-flamingo">Hi.</h2>
               <p>
                   Welcome to my portfolio. Please scroll down.
               </p>
-            </a>
          </div>
       </div>
 
--- components/layout/Socials.js
+++ components/layout/Socials.js
@@ -8,17 +8,11 @@ const Socials = () => {
         <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://facebook.com/catcarbn" target="new">
             <FontAwesomeIcon icon={faFacebook}/>
         </a>
-        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://instagram.com/catvscode" target="new">
+        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://instagram.com/catoutofthepod" target="new">
             <FontAwesomeIcon icon={faInstagram}/>
         </a>
-        <a className="mr-2 w-6 hover:text-custom-flamingo" rel="me" href="https://twitter.com/catvscode" target="new">
+        <a className="mr-2 w-6 hover:text-custom-flamingo" rel="me" href="https://twitter.com/catcarbn" target="new">
             <FontAwesomeIcon icon={faTwitter}/>
-        </a>
-        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://www.pinterest.com/catcarbn/dev-design/" target="new">
-            <FontAwesomeIcon icon={faPinterest}/>
-        </a>
-        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://linkedin.com/in/catcarbonell" target="new">
-            <FontAwesomeIcon icon={faLinkedin}/>
         </a>
         <a className="w-6 hover:text-custom-flamingo" href="https://github.com/catcarbonell" target="new">
             <FontAwesomeIcon icon={faGithub} />
