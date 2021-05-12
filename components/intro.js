8e1403d21d3c1e8e447778f1a6814e4750e09593
catcarbonell <hello@catcarbn.com>
2021-05-12 12:45:55 -0700

    fixed some tailwind stuff

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ const Intro = () =>{
     return(
       <div className="intro w-full lg:w-1/2 md:max-w-xl h-xxs md:mx-auto mt-20 mb-20 flex flex-row rounded-lg">
       
-        <div className="md:w-1/2 p-6 flex flex-col justify-center align-center">
+        <div className="p-6 flex flex-col justify-center align-center">
             <h1 className="text-custom-sakura hover:text-custom-flamingo">Cat Carbonell</h1>
             <h4> UX/Visual Designer &amp; Engineer </h4>
          </div>
