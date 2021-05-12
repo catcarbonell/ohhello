836bb4880e689d667843f5b30a5d267cbcd60432
catcarbonell <hello@catcarbn.com>
2021-05-12 12:45:01 -0700

    changed some CSS

--- components/intro.js
+++ components/intro.js
@@ -7,9 +7,7 @@ const Intro = () =>{
       
         <div className="md:w-1/2 p-6 flex flex-col justify-center align-center">
             <h1 className="text-custom-sakura hover:text-custom-flamingo">Cat Carbonell</h1>
-              <h4>
-                UX/Visual Designer &amp; Engineer
-              </h4>
+            <h4> UX/Visual Designer &amp; Engineer </h4>
          </div>
       </div>
 
