574df44cff2c75c5387eb7a933108f03bd4030de
catcarbonell <hello@catcarbn.com>
2021-05-12 12:43:34 -0700

    changed more header styles and FB link

--- components/intro.js
+++ components/intro.js
@@ -3,13 +3,13 @@ import Link from 'next/link'
 const Intro = () =>{
     
     return(
-      <div className="intro w-full lg:w-1/2 md:max-w-xl h-xxs md:mx-auto mt-20 mb-20 flex flex-row bg-custom-blk bg-opacity-75 rounded-lg">
+      <div className="intro w-full lg:w-1/2 md:max-w-xl h-xxs md:mx-auto mt-20 mb-20 flex flex-row rounded-lg">
       
         <div className="md:w-1/2 p-6 flex flex-col justify-center align-center">
             <h1 className="text-custom-sakura hover:text-custom-flamingo">Cat Carbonell</h1>
-              <h3>
+              <h4>
                 UX/Visual Designer &amp; Engineer
-              </h3>
+              </h4>
          </div>
       </div>
 
--- components/layout/Socials.js
+++ components/layout/Socials.js
@@ -5,10 +5,10 @@ import { faTwitter, faInstagram, faFacebook, faLinkedin, faGithub, faPinterest }
 const Socials = () => {
     return(
     <div className="socials flex flex-row">
-        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://facebook.com/catcarbn" target="new">
+        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://facebook.com/hellooworldco" target="new">
             <FontAwesomeIcon icon={faFacebook}/>
         </a>
-        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://instagram.com/catoutofthepod" target="new">
+        <a className="mr-2 w-6 hover:text-custom-flamingo" href="https://instagram.com/catcarbn" target="new">
             <FontAwesomeIcon icon={faInstagram}/>
         </a>
         <a className="mr-2 w-6 hover:text-custom-flamingo" rel="me" href="https://twitter.com/catcarbn" target="new">
