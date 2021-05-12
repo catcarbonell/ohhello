a365fb327ed8f1a7a9176685db9adcffa38dd8f2
catcarbonell <hello@catcarbn.com>
2021-05-12 12:35:32 -0700

    changed heading sizes

--- components/intro.js
+++ components/intro.js
@@ -6,10 +6,10 @@ const Intro = () =>{
       <div className="intro w-full lg:w-1/2 md:max-w-xl h-xxs md:mx-auto mt-20 mb-20 flex flex-row bg-custom-blk bg-opacity-75 rounded-lg">
       
         <div className="md:w-1/2 p-6 flex flex-col justify-center align-center">
-            <h2 className="text-custom-sakura hover:text-custom-flamingo">Cat Carbonell</h2>
-              <p>
+            <h1 className="text-custom-sakura hover:text-custom-flamingo">Cat Carbonell</h1>
+              <h3>
                 UX/Visual Designer &amp; Engineer
-              </p>
+              </h3>
          </div>
       </div>
 
